d818c6364117d59af99c3cbfb4a4cdc958b36ccd
woo60836 <woo60836@naver.com>
2021-03-17 14:55:55 +0900

    # WARNING: head commit changed in the meantime
    removed Students and added Readings

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
+		System.out.println("1. Add Readings");
+		System.out.println("2. Delete Readings");
+		System.out.println("3. Edit Readings");
+		System.out.println("4. View Readings");
 		System.out.println("5. Exit");
 
 	}
